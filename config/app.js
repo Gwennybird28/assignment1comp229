b6484ffcfb8f35a96fb8a3cc864eed92ea187488
Gwenn <victoriagwenn@gmail.com>
2023-06-21 17:49:38 -0400

    Remove routes to MongoDB

--- config/app.js
+++ config/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('../routes/index');
 var usersRouter = require('../routes/users');
-var productsRouter = require('../routes/products');
 
 var app = express();
 
